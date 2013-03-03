2c2be2c08e48861fc35cff9ae12ca49ef77f24d6
kaspernj <k@spernj.org>
2013-03-03 13:13:58 +0100

    First commit.

---
+++ lib/rails-imager.rb
@@ -0,0 +1,117 @@
+require "RMagick"
+require "knjrbfw"
+require "tmpdir"
+
+class RailsImager
+  IMG_FROM_PARAMS_ALLOWED_ARGS = [:image, :params]
+  CACHENAME_FROM_PARAMS_ALLOWED_ARGS = [:params]
+  PARAMS_ARGS = [:width, :height, :smartsize, :maxwidth, :maxheight, :rounded_corners, :border, :border_color]
+  INITIALIZE_ALLOWED_ARGS = [:cache_dir]
+  
+  DEFAULT_CACHE_DIR = "#{Dir.tmpdir}/rails-imager-cache"
+  Dir.mkdir(DEFAULT_CACHE_DIR) if !Dir.exists?(DEFAULT_CACHE_DIR)
+  
+  DEFAULT_ARGS = {
+    :cache_dir => DEFAULT_CACHE_DIR
+  }
+  
+  def initialize(args = {})
+    args.each do |key, val|
+      raise "Invalid argument: '#{key}'." if !INITIALIZE_ALLOWED_ARGS.include?(key)
+    end
+    
+    @args = DEFAULT_ARGS.merge(args)
+  end
+  
+  def img_from_params(args)
+    args.each do |key, val|
+      raise "Invalid argument: '#{key}'." if !IMG_FROM_PARAMS_ALLOWED_ARGS.include?(key)
+    end
+    
+    #Set up vars.
+    img = args[:image]
+    raise "No image given." if !img
+    raise "Wrong image-object given: '#{img.class.name}'." if !img.is_a?(Magick::Image)
+    
+    params = args[:params]
+    raise "No params given." if !params
+    
+    img_width = img.columns
+    img_height = img.rows
+    
+    width = img_width
+    height = img_height
+    
+    width = params[:width] if params[:width]
+    height = params[:height] if params[:height]
+    
+    #Check arguments and manipulate image.
+    if params[:smartsize]
+      if img_width > img_height
+        width = params[:smartsize].to_i
+        height = (img_height.to_f / (img_width.to_f / width.to_f)).to_i
+      else
+        height = params[:smartsize].to_i
+        width = (img_width.to_f / (img_height.to_f / height.to_f)).to_i
+      end
+    end
+    
+    if params[:maxwidth]
+      maxwidth = params[:maxwidth].to_i
+      
+      if width > maxwidth
+        height = (img_height.to_f / (img_width.to_f / maxwidth.to_f)).to_i
+        width = maxwidth
+      end
+    end
+    
+    if params[:maxheight]
+      maxheight = params[:maxheight].to_i
+      
+      if height > maxheight
+        width = (img_width.to_f / (img_height.to_f / maxheight.to_f)).to_i
+        height = maxheight
+      end
+    end
+    
+    if params[:width] and params[:height]
+      width = params[:width].to_i
+      height = params[:height].to_i
+    end
+    
+    if width != img_width or height != img_height
+      img = img.resize(width, height)
+    end
+    
+    if params[:rounded_corners]
+      img = img.clone
+      args = {:img => img, :radius => params[:rounded_corners].to_i}
+      
+      if params[:border] and _params[:border_color]
+        args[:border] = params[:border]
+        args[:border_color] = params[:border_color]
+      end
+      
+      Knj::Image.rounded_corners(args)
+    end
+    
+    return img
+  end
+  
+  def cachename_from_params(args)
+    args.each do |key, val|
+      raise "Invalid argument: '#{key}'." if !CACHENAME_FROM_PARAMS_ALLOWED_ARGS.include?(key)
+    end
+    
+    params = args[:params]
+    raise "No params was given." if !params
+    
+    name = ""
+    PARAMS_ARGS.each do |val|
+      name += "__" if !name.empty?
+      name += "#{val}_#{params[val]}"
+    end
+    
+    return name
+  end
+end
--- spec/rails-imager_spec.rb
+++ spec/rails-imager_spec.rb
@@ -1,7 +1,60 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+require "tmpdir"
+require "fileutils"
+
 describe "RailsImager" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  TEST_FILE = File.realpath("#{File.dirname(__FILE__)}/test.png")
+  IMG = Magick::Image.read(TEST_FILE).first
+  CACHE_DIR = "#{Dir.tmpdir}/rails-imager-test-cache"
+  FileUtils.rm_r(CACHE_DIR) if Dir.exists?(CACHE_DIR)
+  Dir.mkdir(CACHE_DIR)
+  RIMG = RailsImager.new(:cache_dir => CACHE_DIR)
+  
+  it "should do smartsizing" do
+    newimg = RIMG.img_from_params(:image => IMG, :params => {:smartsize => 640})
+    newimg.columns.should eql(640)
+    newimg.rows.should eql(629)
+  end
+  
+  it "should do exact sizes" do
+    newimg = RIMG.img_from_params(:image => IMG, :params => {:width => 640, :height => 480})
+    newimg.columns.should eql(640)
+    newimg.rows.should eql(480)
+  end
+  
+  it "should do rounded corners" do
+    newimg = RIMG.img_from_params(:image => IMG, :params => {:smartsize => 640, :rounded_corners => 15})
+    
+    newimg.columns.should eql(640)
+    newimg.rows.should eql(629)
+    
+    #Test that corner pixels are transparent.
+    2.times do |time|
+      pixel = newimg.pixel_color(time, time)
+      pixel.opacity.should eql(65535)
+      
+      pixel_orig = IMG.pixel_color(time, time)
+      pixel_orig.opacity.should eql(0)
+    end
+    
+    #Test that middle pixels are not transparent.
+    100.upto(200) do |time|
+      pixel = newimg.pixel_color(time, time)
+      pixel.opacity.should eql(0)
+      
+      pixel_orig = IMG.pixel_color(time, time)
+      pixel_orig.opacity.should eql(0)
+    end
+  end
+  
+  it "should do max width and height" do
+    newimg = RIMG.img_from_params(:image => IMG, :params => {:maxwidth => 200})
+    newimg.columns.should eql(200)
+    newimg.rows.should eql(196)
+    
+    newimg = RIMG.img_from_params(:image => IMG, :params => {:maxheight => 200})
+    newimg.rows.should eql(200)
+    newimg.columns.should eql(203)
   end
 end
