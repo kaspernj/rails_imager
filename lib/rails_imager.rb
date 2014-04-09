34068fefec6d0493a1afdde38599d2309821e2f0
Kasper Johansen <k@spernj.org>
2014-04-09 13:43:53 +0200

    Replaced __dir__ with File.dirname(__FILE__) to support 1.9

--- lib/rails_imager.rb
+++ lib/rails_imager.rb
@@ -3,11 +3,11 @@ require "rails_imager/engine"
 module RailsImager
   def self.const_missing(name)
     if name.to_s.end_with?("Controller")
-      path = "#{__dir__}/../app/controllers/rails_imager/#{StringCases.camel_to_snake(name)}"
+      path = "#{File.dirname(__FILE__)}/../app/controllers/rails_imager/#{StringCases.camel_to_snake(name)}"
     elsif name.to_s.end_with?("Helper")
-      path = "#{__dir__}/../app/helpers/rails_imager/#{StringCases.camel_to_snake(name)}"
+      path = "#{File.dirname(__FILE__)}/../app/helpers/rails_imager/#{StringCases.camel_to_snake(name)}"
     else
-      path = "#{__dir__}/rails_imager/#{StringCases.camel_to_snake(name)}"
+      path = "#{File.dirname(__FILE__)}/rails_imager/#{StringCases.camel_to_snake(name)}"
     end
     
     require path
