352b49ad5732f0a59a575a61a86bdc00898960e6
Kasper Johansen <k@spernj.org>
2014-05-25 12:40:26 +0200

    Fixed spec to not include "/".

--- spec/helpers/rails_imager/images_helper_spec.rb
+++ spec/helpers/rails_imager/images_helper_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe RailsImager::ImagesHelper do
   it "#rails_imager_path" do
     path = rails_imager_p("test.png", :smartsize => 200)
-    assert_equal "/rails_imager/images/test.png/?image[smartsize]=200", path
+    assert_equal "/rails_imager/images/test.png?image[smartsize]=200", path
   end
 end
